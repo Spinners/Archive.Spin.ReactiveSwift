b4d9d7c109446bc0e235dab7acf8222a891fb3aa
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-08-17 18:31:17 -0400

    Initial Commit

---
+++ Spin.ReactiveSwift/Spin_ReactiveSwift.h
@@ -0,0 +1,19 @@
+//
+//  Spin_ReactiveSwift.h
+//  Spin.ReactiveSwift
+//
+//  Created by Thibault Wittemberg on 2019-08-17.
+//  Copyright © 2019 WarpFactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Spin_ReactiveSwift.
+FOUNDATION_EXPORT double Spin_ReactiveSwiftVersionNumber;
+
+//! Project version string for Spin_ReactiveSwift.
+FOUNDATION_EXPORT const unsigned char Spin_ReactiveSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Spin_ReactiveSwift/PublicHeader.h>
+
+
