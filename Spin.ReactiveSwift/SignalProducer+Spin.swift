31622936f84a040c31fee80b5b95232de85d51b6
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-08-17 20:29:05 -0400

    project: add base files

---
+++ Spin.ReactiveSwift/SignalProducer+Spin.swift
@@ -0,0 +1,41 @@
+//
+//  SignalProducer+Spin.swift
+//  Spin.ReactiveSwift
+//
+//  Created by Thibault Wittemberg on 2019-08-17.
+//  Copyright © 2019 WarpFactor. All rights reserved.
+//
+
+import ReactiveSwift
+import Spin
+
+extension SignalProducer: Producer & Consumable {
+    public typealias Input = SignalProducer
+    public typealias Value = Value
+    public typealias Context = Scheduler
+    public typealias Runtime = Disposable
+
+    public static func from(function: () -> Input) -> AnyProducer<Input, Value, Context, Runtime> {
+        return function().eraseToAnyProducer()
+    }
+
+    public func compose<Output: Producer>(function: (Input) -> Output) -> AnyProducer<Output.Input, Output.Value, Output.Context, Output.Runtime> {
+        return function(self).eraseToAnyProducer()
+    }
+
+    public func scan<Result>(initial value: Result, reducer: @escaping (Result, Value) -> Result) -> AnyConsumable<Result, Context, Runtime> {
+        return self.scan(value, reducer).eraseToAnyConsumable()
+    }
+
+    public func consume(by: @escaping (Value) -> Void, on: Context) -> AnyConsumable<Value, Context, Runtime> {
+        return self.observe(on: on).on(value: by).eraseToAnyConsumable()
+    }
+
+    public func spy(function: @escaping (Value) -> Void) -> AnyProducer<Input, Value, Context, Runtime> {
+        return self.on(value: function).eraseToAnyProducer()
+    }
+
+    public func engage() -> Runtime {
+        return self.start()
+    }
+}
